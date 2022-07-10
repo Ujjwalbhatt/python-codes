a29ced1dc624a48c91528979df64fd44d947ea32
Ujjwal <Ujjwalbhatt04@gmail.com>
2022-07-10 18:42:35 +0530

    gffg

--- edfds.py
+++ edfds.py
@@ -1,15 +1,12 @@
 # write a python to read 4 number from the user and find the greatest from the user
+a,b,c,d=map(int,input("Enter the four number: ").split())
 
-n=int(input("Enter the number: "))
-n1=int(input("Enter the number: "))
-n2=int(input("Enter the number: "))
-n3=int(input("Enter the number: "))
-if (n>n1 and n>n2 and n>n3):
-        print(n , " is the greatest")
-elif (n1>n and n1>n2 and n1>n3):
-        print(n1 , " is the greatest")
-elif (n2>n1 and n2>n and n2>n3):
-        print(n2 , " is the greatest")
-else:
-        print(n3 , " is the greatest")
-    
+big=a
+if (b>big):
+        big=b
+if (c>big):
+        big=c
+if (d>big):
+        big=d
+print("the highest number is: ", big)
+        
---
+++ ergd.py
@@ -0,0 +1,7 @@
+# to read the number from the user echange the first digit with last digit
+a=int(input("Enter the multidigit number: "))
+i=0
+while(a>0):
+      n=a%10
+      
+ 
